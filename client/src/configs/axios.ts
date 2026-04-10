01a445c2a9cef70325b4bed0efed99090386473e
DevNathani <nathanidb313@gmail.com>
2026-04-10 20:34:16 +0530

    bugs

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'https://creatix-backend.onrender.com',
+    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3000',
     withCredentials: true,
     timeout: 30000, // 30 second timeout
     headers: {
